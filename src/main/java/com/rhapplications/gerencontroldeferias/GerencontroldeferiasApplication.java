39e85445a349e1581e92eba9c63a4044ab800f5f
pppaiva <pedropaiva.62124@gmail.com>
2025-01-29 15:59:54 -0300

    subindo a estrutura inicial do projeto criando conexão com o repo-remoto

---
+++ src/main/java/com/rhapplications/gerencontroldeferias/GerencontroldeferiasApplication.java
@@ -0,0 +1,13 @@
+package com.rhapplications.gerencontroldeferias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencontroldeferiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerencontroldeferiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhapplications/gerencontroldeferias/GerencontroldeferiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhapplications.gerencontroldeferias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerencontroldeferiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
