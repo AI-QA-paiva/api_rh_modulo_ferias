421f11777c468809b167a3bd18c3b34b4be2b0ab
pppaiva <pedropaiva.62124@gmail.com>
2025-01-29 16:19:59 -0300

    criação das classes model com relacionamentos e enums para constantes

---
+++ src/main/java/com/rhapplications/gerencontroldeferias/enumeration/ProfileEnum.java
@@ -0,0 +1,6 @@
+package com.rhapplications.gerencontroldeferias.enumeration;
+
+public enum ProfileEnum {
+    ADMIN,
+    EMPLOYEE
+}
---
+++ src/main/java/com/rhapplications/gerencontroldeferias/enumeration/StatusVacationRequest.java
@@ -0,0 +1,8 @@
+package com.rhapplications.gerencontroldeferias.enumeration;
+
+public enum StatusVacationRequest {
+    CREATED,
+    ONGOING,
+    CONCLUDED,
+    CANCELED
+}
